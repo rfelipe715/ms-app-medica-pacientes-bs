8ba9c61dc825ab0221010132459dd988113512e8
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-06-06 09:51:20 -0400

    primer commit

---
+++ src/main/java/cl/duoc/ms_pacientes_bs/MsPacientesBsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_pacientes_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPacientesBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPacientesBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_pacientes_bs/MsPacientesBsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_pacientes_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPacientesBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
